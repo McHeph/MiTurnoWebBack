6a4f0eed65d46818fef8dfaa6ea2fc55db7937f5
Juan Arismendi <juanarismendidiaz@gmail.com>
2024-05-27 18:20:41 -0300

    adapting to vercel deployment

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use("/api", routes);
 // Ruta para la documentación de Swagger
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-db.sync({ force: false }).then(() => {
+db.sync({ force: true }).then(() => {
   app.listen(5001, () => console.log(`Servidor  en el puerto 5001`));
 });
 module.exports = app;
