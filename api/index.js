21ff853d39b6350dfa91ea4764997c4f61e0a5c9
Juan Arismendi <juanarismendidiaz@gmail.com>
2024-05-29 19:12:31 -0300

    Change CORS origin

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://miturnoweb.vercel.app/",
     credentials: true,
   })
 );
